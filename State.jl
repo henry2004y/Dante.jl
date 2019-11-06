e6c72a437234489bf794dacad547fc4a4bb84c17
hyzhou <hyzhou@umich.edu>
2019-11-06 00:37:34 -0500

    Added set_init_Riemann.

--- State.jl
+++ State.jl
@@ -1,12 +1,10 @@
 module State
 
-export set_init, update_state!, plotvar
+export set_init, set_init_Riemann, update_state!
 
 using ..Parameters: Param, Rho_, Ux_, Uy_, Uz_, Bx_, By_, Bz_, P_, E_, U_, B_
 using ..Parameters: γ
 using ..Flux: FaceFlux
-
-using PyPlot
 
 
 function set_init(param::Param)
@@ -46,93 +44,8 @@ function set_init(param::Param)
       velocity .*= density
       pressure .= 0.01
    elseif param.IC == "Riemann"
-      # Make this into a separate function!
-      if param.RiemannProblemType == 1
-         println("Case 1: Sods problem")
-         p   = [1.0, 0.1]
-         u   = [0.0, 0.0]
-         rho = [1.0, 0.125]
-         tEnd, CFL = 0.1, 0.9 # I need to think of how to set this!!!
-      elseif param.RiemannProblemType == 2
-         println("Case 2: Left Expansion and right strong shock")
-         p   = [1000.0, 0.1]
-         u   = [0.0   , 0.0]
-         rho = [3.0   , 2.0]
-         tEnd, CFL = 0.02, 0.9
-      elseif param.RiemannProblemType == 3
-         println("Case 3: Right Expansion and left strong shock")
-         p   = [7.0, 10.0]
-         u   = [0.0, 0.0 ]
-         rho = [1.0, 1.0 ]
-         tEnd, CFL = 0.1, 0.9
-      elseif param.RiemannProblemType == 4
-         println("Case 4: Double Shock")
-         p   = [450.0, 45.0  ]
-         u   = [20.0 , -6.0  ]
-         rho = [6.0  ,  6.0  ]
-         tEnd, CFL = 0.01, 0.90
-      elseif param.RiemannProblemType == 5
-         println("Case 5: Double Expansion")
-         p   = [40.0,  40.0 ]
-         u   = [-2.0,  2.0  ]
-         rho = [1.0 ,  2.5  ]
-         tEnd, CFL = 0.03, 0.90
-      elseif param.RiemannProblemType == 6
-         println("Case 6: Cavitation")
-         p   = [0.4 , 0.4]
-         u   = [-2.0, 2.0]
-         rho = [ 1.0, 1.0]
-         tEnd, CFL = 0.1, 0.90
-      elseif param.RiemannProblemType == 7
-         println("Shocktube problem of G.A. Sod, JCP 27:1, 1978")
-         p   = [1.0 , 0.1  ]
-         u   = [0.75, 0.0  ]
-         rho = [1.0 , 0.125]
-         tEnd, CFL = 0.17, 0.90
-      elseif param.RiemannProblemType == 8
-         println("Lax test case: M. Arora and P.L. Roe: JCP 132:3-11, 1997")
-         p   = [3.528, 0.571]
-         u   = [0.698, 0    ]
-         rho = [0.445, 0.5  ]
-         tEnd, CFL = 0.15, 0.90
-      elseif param.RiemannProblemType == 9
-         println("Mach = 3 test case: M. Arora and P.L. Roe: JCP 132:3-11, 1997")
-         p   = [10.333,  1.0  ]
-         u   = [ 0.92 ,  3.55 ]
-         rho = [3.857 ,  1.0  ]
-         tEnd, CFL = 0.09, 0.90
-      elseif param.RiemannProblemType == 10
-         println("Shocktube problem with supersonic zone")
-         p   = [1.0,  0.02]
-         u   = [0.0,  0.00]
-         rho = [1.0,  0.02]
-         tEnd, CFL = 0.162, 0.90
-      elseif param.RiemannProblemType == 11
-         println("Contact discontinuity")
-         p   = [0.5, 0.5]
-         u   = [0.0, 0.0]
-         rho = [1.0, 0.6]
-         tEnd, CFL = 1.0, 0.90
-      elseif param.RiemannProblemType == 12
-         println("Stationary shock")
-         p   = [ 1.0,  0.1 ]
-         u   = [-2.0, -2.0 ]
-         rho = [ 1.0, 0.125]
-         tEnd, CFL = 0.1, 0.28
-      else
-         error("RiemannProblemType not known!")
-      end
-      # Print for Riemann Problems
-      println("")
-      println("density (L): $(rho[1])")
-      println("velocity(L): $(u[1])")
-      println("Pressure(L): $(p[1])")
-      println("")
-      println("density (R): $(rho[2])")
-      println("velocity(R): $(u[2])")
-      println("Pressure(R): $(p[2])")
-
-      # Initial Condition for our 1D domain
+      rho, u, p, tEnd, CFL = set_init_Riemann(param.RiemannProblemType)
+      # Initial Condition for 1D domain
       # Density
       density[1:floor(Int,nI/2)+nG+1,:,:] .= rho[1] # region 1
       density[floor(Int,nI/2)+nG+2:end,:,:] .= rho[2] # region 2
@@ -148,6 +61,101 @@ function set_init(param::Param)
    end
 
    return state_GV
+end
+
+"""
+   set_init_Riemann(RiemannProblemType)
+Set the initial conditions of Riemann problems. Note that currently tEnd and CFL
+number can only be set in PARAM.toml, and cannot be changed afterwards.
+"""
+function set_init_Riemann(RiemannProblemType::Int)
+
+   if RiemannProblemType == 1
+      println("Case 1: Sods problem")
+      p   = [1.0, 0.1]
+      u   = [0.0, 0.0]
+      rho = [1.0, 0.125]
+      tEnd, CFL = 0.1, 0.9 # I need to think of how to set this!!!
+   elseif RiemannProblemType == 2
+      println("Case 2: Left Expansion and right strong shock")
+      p   = [1000.0, 0.1]
+      u   = [0.0   , 0.0]
+      rho = [3.0   , 2.0]
+      tEnd, CFL = 0.02, 0.9
+   elseif RiemannProblemType == 3
+      println("Case 3: Right Expansion and left strong shock")
+      p   = [7.0, 10.0]
+      u   = [0.0, 0.0 ]
+      rho = [1.0, 1.0 ]
+      tEnd, CFL = 0.1, 0.9
+   elseif RiemannProblemType == 4
+      println("Case 4: Double Shock")
+      p   = [450.0, 45.0]
+      u   = [20.0 , -6.0]
+      rho = [6.0  ,  6.0]
+      tEnd, CFL = 0.01, 0.90
+   elseif RiemannProblemType == 5
+      println("Case 5: Double Expansion")
+      p   = [40.0,  40.0]
+      u   = [-2.0,  2.0 ]
+      rho = [1.0 ,  2.5 ]
+      tEnd, CFL = 0.03, 0.90
+   elseif RiemannProblemType == 6
+      println("Case 6: Cavitation")
+      p   = [0.4 , 0.4]
+      u   = [-2.0, 2.0]
+      rho = [ 1.0, 1.0]
+      tEnd, CFL = 0.1, 0.90
+   elseif RiemannProblemType == 7
+      println("Shocktube problem of G.A. Sod, JCP 27:1, 1978")
+      p   = [1.0 , 0.1  ]
+      u   = [0.75, 0.0  ]
+      rho = [1.0 , 0.125]
+      tEnd, CFL = 0.17, 0.90
+   elseif RiemannProblemType == 8
+      println("Lax test case: M. Arora and P.L. Roe: JCP 132:3-11, 1997")
+      p   = [3.528, 0.571]
+      u   = [0.698, 0    ]
+      rho = [0.445, 0.5  ]
+      tEnd, CFL = 0.15, 0.90
+   elseif RiemannProblemType == 9
+      println("Mach = 3 test case: M. Arora and P.L. Roe: JCP 132:3-11, 1997")
+      p   = [10.333,  1.0 ]
+      u   = [ 0.92 ,  3.55]
+      rho = [3.857 ,  1.0 ]
+      tEnd, CFL = 0.09, 0.90
+   elseif RiemannProblemType == 10
+      println("Shocktube problem with supersonic zone")
+      p   = [1.0,  0.02]
+      u   = [0.0,  0.00]
+      rho = [1.0,  0.02]
+      tEnd, CFL = 0.162, 0.90
+   elseif RiemannProblemType == 11
+      println("Contact discontinuity")
+      p   = [0.5, 0.5]
+      u   = [0.0, 0.0]
+      rho = [1.0, 0.6]
+      tEnd, CFL = 1.0, 0.90
+   elseif RiemannProblemType == 12
+      println("Stationary shock")
+      p   = [ 1.0,  0.1 ]
+      u   = [-2.0, -2.0 ]
+      rho = [ 1.0, 0.125]
+      tEnd, CFL = 0.1, 0.28
+   else
+      error("RiemannProblemType not known!")
+   end
+   # Print for Riemann Problems
+   println("")
+   println("density (L): $(rho[1])")
+   println("velocity(L): $(u[1])")
+   println("Pressure(L): $(p[1])")
+   println("")
+   println("density (R): $(rho[2])")
+   println("velocity(R): $(u[2])")
+   println("Pressure(R): $(p[2])")
+
+   return rho, u, p, tEnd, CFL
 end
 
 """Time-accurate version."""
@@ -272,38 +280,6 @@ function update_state!(param::Param, state_GV::Array{Float64,4},
       state_GV .= 0.0
    end
 
-end
-
-function plotvar(param::Param, it::Int, state_GV::Array{Float64,4})
-
-   # Now this only works for 1D x!
-   iMin, iMax, jMin, jMax, kMin, kMax =
-   param.iMin, param.iMax, param.jMin, param.jMax, param.kMin, param.kMax
-
-   plotvar = param.PlotVar
-   nG = param.nG
-
-   if plotvar == "rho"
-      var = @view state_GV[:,:,:,Rho_]
-   elseif plotvar == "ux"
-      var = @view state_GV[:,:,:,Ux_]
-   elseif plotvar == "p"
-      var = @view state_GV[:,:,:,P_]
-   else
-      error("unknown plotting varname!")
-   end
-
-   x = param.x[1+nG:end-nG]
-
-   var = @view var[iMin:iMax,jMin:jMax,kMin:kMax] # Remove ghost cells
-
-   var = dropdims(var; dims=(2,3))
-
-   #figure()
-   plot(x,var,marker=".")
-
-   title("iStep=$(it)")
-   legend(labels=[plotvar])
 end
 
 end
