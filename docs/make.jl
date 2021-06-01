427720a1af5d0db6fd3a4c9c06bbf28d24a3a750
Hongyang Zhou <hyzhou@umich.edu>
2021-06-01 14:55:50 +0300

    deploy docs

--- docs/make.jl
+++ docs/make.jl
@@ -20,9 +20,6 @@ makedocs(
     ]
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(;
+    repo="github.com/henry2004y/Dante.jl",
+)
